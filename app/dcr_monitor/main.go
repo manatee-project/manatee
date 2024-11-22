bc3e696ddbe1573269d6098cd3685162af236033
privacywill <privacywill96@gmail.com>
2024-11-22 14:23:33 +0800

    move config file to deploy stage (#33)
    This PR solves https://github.com/manatee-project/manatee/issues/4
    ## Changes
    - make the api and monitor to read configs from environment variables. 
    - remove `pkg/config`.
    - remove `pkg/conf` in bazel. 
    - add a `--debug` parameters to `deploy.sh`. By default `debug` set to
    false. If the `debug` set to true, the confidential space won't be
    terminated, so the job status should always be `VM Running`. You can
    delete the instance manually at gcp console.
    
    ## Caveats
    - You should delete the old pod to make the new environment variable
    take effects. `kubectl delete pod -n $namespace` or `helm uninstall
    data-clean-room-helm -n $namespace`
    ## Testing
    - Manual end-to-end testing. `./deploy.sh --namespace=xx --debug=true`
    works as expected.

--- app/dcr_monitor/main.go
+++ app/dcr_monitor/main.go
@@ -2,21 +2,13 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/cloudwego/hertz/pkg/common/hlog"
 	"github.com/manatee-project/manatee/app/dcr_api/biz/dal/db"
-	"github.com/manatee-project/manatee/pkg/config"
 )
 
 func main() {
-
-	err := config.InitConfig()
-	if err != nil {
-		fmt.Printf("ERROR: failed to init config %+v \n", err)
-		panic(err)
-	}
 
 	ctx := context.Background()
 
